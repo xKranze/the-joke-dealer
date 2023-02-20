d32535c07e14a5ce8efa3676d3539be1d52187db
Jimmy Yin <118097335+xKranze@users.noreply.github.com>
2023-02-19 16:08:27 -0800

    added joke API and functions

--- assets/script.js
+++ assets/script.js
@@ -1,10 +1,70 @@
 // alert("Some jokes can be innapropriate, Click OK if you are 18+ of age!");
 
-const notification =document.getElementById('notification');
-const notificationClass= document.getElementById('div.notification');
+const notification = document.getElementById('notification');
+var jokes;
+notification.addEventListener('click', () => {
+    notification.remove();
+});
 
-notification.addEventListener('click',() => {
-notification.classList.remove('notification')
-notificationClass.classList.remove('notificationClass')
+document.getElementById("searchBtn").addEventListener('click', (e) => {
+    e.preventDefault();
+    var categories = document.getElementById("selectCat").value;
+    var search = document.getElementById("searchString").value;
+    var jokeType = document.getElementById("jokeType").value;
+    if (jokeType !=""){
+        jokeType += "&";
+    }
+    var urlAPI = "https://v2.jokeapi.dev/joke/" + categories + "?"+ jokeType+"contains=" + search + "&amount=10";
 
-});  
+    fetch(urlAPI)
+    .then((response) => response.json())
+    .then(function(data){
+        console.log(data);
+        if(data){
+            jokes = data.jokes;
+            for (i=0; i<data.jokes.length; i++){
+                if(data.jokes[i].type=="single")
+                    document.getElementById("jokesDisplay").innerHTML +=
+                    "<div class='card m-3 has-background-link-light' style='padding:12px;'>" + 
+                    data.jokes[i].joke + 
+                    "<br><br><button onclick='addFavorite("+i+");'>Add to Favorites</button>" + 
+                    "</div><hr>";
+                else{
+                    document.getElementById("jokesDisplay").innerHTML += 
+                    "<div class='card m-3 has-background-link-light' style='padding:12px;'>" + 
+                    data.jokes[i].setup + "<br>" + 
+                    data.jokes[i].delivery + 
+                    "<br><br><button onclick='addFavorite("+i+");'>Add to Favorites</button>" + 
+                    "</div><hr>";
+                }
+            }
+            if(data.jokes.length>0){
+
+            }
+        }
+    
+    })
+});
+// jokeNumber is recieved/results from line 30 and 37 +i+" [i] is part of the array which we get from the web api side.
+function addFavorite(jokeNumber){
+    console.log(jokes[jokeNumber]);
+    if(jokes[jokeNumber].type=="single"){
+        localStorage.setItem()
+        // joke
+    }else{
+        // setup, delivery
+    }
+}
+
+document.getElementById("jokesDisplay").innerHTML = joke +  document.getElementById("jokesDisplay").innerHTML
+
+document.getElementById("clearJokesBtn").addEventListener('click', (e) => {
+    e.preventDefault();
+    document.getElementById("jokesDisplay").innerHTML = "";
+});
+
+document.getElementById("clearFavBtn").addEventListener('click', (e) => {
+    e.preventDefault();
+    //document.getElementById("jokesDisplay").innerHTML = "";
+});
+
